ff13ac713e073aa27751a2c85a99232c8bbd1d69
Rico Simlinger <r.simlinger@secupay.ag>
2017-06-07 15:50:21 +0200

    Release/1.1.0 (#2)
    * Fix for SCCON-97
    
    * REMOVE deprecated tests (currently not all tests are up-to-date and working)
    
    * Move license information from files to README.md and LICENSE.md
    
    * ADD new Smart.Transaction methods: diagnosis, endOfDay, cancelPayment
    
    * RELEASE 1.1.0
    
    * Fix for SCCON-97

---
+++ lib/Secucard.Connect/Product/Smart/Model/ReceiptNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace Secucard.Connect.Product.Smart.Model
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class ReceiptNumber
+    {
+        [DataMember(Name = "receipt_number")]
+        public string receiptNumber { get; set; }
+
+        public override string ToString()
+        {
+            return "ReceiptNumber{" +
+                   "receipt_number='" + receiptNumber +
+                   '}';
+        }
+    }
+}
--- lib/Secucard.Connect/Product/Smart/TransactionsService.cs
+++ lib/Secucard.Connect/Product/Smart/TransactionsService.cs
@@ -1,16 +1,4 @@
-﻿/*
- * Copyright (c) 2015. hp.weber GmbH & Co secucard KG (www.secucard.com)
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-namespace Secucard.Connect.Product.Smart
+﻿namespace Secucard.Connect.Product.Smart
 {
     using Secucard.Connect.Client;
     using Secucard.Connect.Net;
@@ -18,6 +6,7 @@ namespace Secucard.Connect.Product.Smart
     using Secucard.Connect.Product.General.Model;
     using Secucard.Connect.Product.Smart.Event;
     using Transaction = Secucard.Connect.Product.Smart.Model.Transaction;
+    using ReceiptNumber = Secucard.Connect.Product.Smart.Model.ReceiptNumber;
 
     public class TransactionsService : ProductService<Transaction>
     {
@@ -38,14 +27,14 @@ namespace Secucard.Connect.Product.Smart
 
         protected override ServiceMetaData<Transaction> GetMetaData()
         {
-             return MetaData; 
+            return MetaData;
         }
 
         private void OnNewEvent(object obj)
         {
             if (TransactionCashierEvent != null)
                 TransactionCashierEvent(this,
-                    new TransactionCashierEventArgs {SecucardEvent = (Event<Notification>) obj});
+                    new TransactionCashierEventArgs { SecucardEvent = (Event<Notification>)obj });
         }
 
         /// <summary>
@@ -76,11 +65,11 @@ namespace Secucard.Connect.Product.Smart
             }
 
             return Execute<Transaction>(transactionId, "start", type, null,
-                new ChannelOptions {Channel = channel });
+                new ChannelOptions { Channel = channel });
         }
 
         /// <summary>
-        /// Cancel the existing transaction with the given transactionId.
+        /// Cancel the existing (Loyalty) transaction with the given transactionId.
         /// </summary>
         /// <param name="transactionId">Cancel the specific transaction by id</param>
         /// <returns></returns>
@@ -88,5 +77,36 @@ namespace Secucard.Connect.Product.Smart
         {
             return ExecuteToBool(transactionId, "cancel", null, null, null);
         }
+
+        /// <summary>
+        /// Starts extended Diagnose.
+        /// </summary>
+        /// <param name="transactionId">Starts the specific transaction by id</param>
+        /// <returns></returns>
+        public Transaction Diagnosis()
+        {
+            return Execute<Transaction>(null, "diagnosis", null, null, new ChannelOptions { Channel = ChannelOptions.ChannelStomp });
+        }
+
+        /// <summary>
+        /// Starts End of Day Report (Kassenschnitt).
+        /// </summary>
+        /// <returns></returns>
+        public Transaction EndOfDay()
+        {
+            return Execute<Transaction>(null, "endOfDay", null, null, new ChannelOptions { Channel = ChannelOptions.ChannelStomp });
+        }
+
+        /// <summary>
+        /// Cancel payment transaction (different from Loyalty).
+        /// </summary>
+        /// <param name="receiptNumber">Receipt number to cancel</param>
+        /// <returns></returns>
+        public Transaction CancelPayment(string receiptNumber)
+        {
+            return Execute<Transaction>(null, "cancelTrx", null, new ReceiptNumber { receiptNumber = receiptNumber }, new ChannelOptions { Channel = ChannelOptions.ChannelStomp });
+        }
+
     }
+
 }
