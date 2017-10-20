4d1db6f00678a0b6579a48b150d5259f91cd7a71
Rico Simlinger <r.simlinger@secupay.ag>
2017-10-20 10:12:21 +0200

    FIX missing constructor

--- lib/Secucard.Connect/Client/SecucardConnectException.cs
+++ lib/Secucard.Connect/Client/SecucardConnectException.cs
@@ -4,7 +4,15 @@
 
     public class SecucardConnectException : Exception
     {
+        public SecucardConnectException() : base()
+        {
+        }
+
         public SecucardConnectException(string message) : base(message)
+        {
+        }
+
+        public SecucardConnectException(string message, Exception innerException) : base(message, innerException)
         {
         }
     }
