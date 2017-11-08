728176fbc3c80d862eba68130c289109610f64a4
André Mommert <a.mommert@secupay.ag>
2017-11-08 13:00:11 +0100

    Added documentation

--- lib/Secucard.Connect/Product/Loyalty/CardGroupsService.cs
+++ lib/Secucard.Connect/Product/Loyalty/CardGroupsService.cs
@@ -6,6 +6,13 @@
 
     public class CardGroupsService : ProductService<CardGroup>
     {
+        /// <summary>
+        /// Check if a passcode is enabled for this card
+        /// </summary>
+        /// <param name="cardGroupId">CRG_XYZ</param>
+        /// <param name="transactionType">Type of transaction</param>
+        /// <param name="cardNumber">Number of the card</param>
+        /// <returns></returns>
         public bool CheckPasscodeEnabled(string cardGroupId, string transactionType, string cardNumber)
         {
             var data = new { action = transactionType, cardnumber = cardNumber };
--- lib/Secucard.Connect/Product/Loyalty/MerchantCardsService.cs
+++ lib/Secucard.Connect/Product/Loyalty/MerchantCardsService.cs
@@ -8,12 +8,24 @@ namespace Secucard.Connect.Product.Loyalty
         public static readonly ServiceMetaData<MerchantCard> MetaData = new ServiceMetaData<MerchantCard>(
             "loyalty", "merchantcards");
 
+        /// <summary>
+        /// Check the given CSC
+        /// </summary>
+        /// <param name="cardNumber">Number of the card</param>
+        /// <param name="csc">CSC number</param>
+        /// <returns></returns>
         public bool ValidateCSC(string cardNumber, string csc)
         {
             var data = new { cardnumber = cardNumber, csc = csc };
             return this.ExecuteToBool("me", "CheckCsc", null, data, null);
         }
 
+        /// <summary>
+        /// Check the given passcode
+        /// </summary>
+        /// <param name="cardNumber">Number of the card</param>
+        /// <param name="pin">PIN number</param>
+        /// <returns></returns>
         public bool ValidatePasscode(string cardNumber, string pin)
         {
             var data = new { cardnumber = cardNumber, pin = pin };
