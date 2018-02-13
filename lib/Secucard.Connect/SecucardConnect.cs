2c9e11d3435e345b89ef99ed10b375fc2379cb6d
Rico Simlinger <r.simlinger@secupay.ag>
2018-02-13 11:59:48 +0100

    ADD UrlPush param

--- lib/Secucard.Connect/SecucardConnect.cs
+++ lib/Secucard.Connect/SecucardConnect.cs
@@ -25,7 +25,7 @@ namespace Secucard.Connect
     /// </summary>
     public class SecucardConnect
     {
-        public const string Version = "1.4.0";
+        public const string Version = "1.4.1";
         private readonly ClientConfiguration _configuration;
         private readonly ClientContext _context;
         private readonly Dictionary<string, IService> _serviceDict;
